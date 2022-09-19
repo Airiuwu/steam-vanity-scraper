4fe325c775054e71ad8f6c2d557b8f44a3f06ebe
Jacob <61052022+Airiuwu@users.noreply.github.com>
2022-09-19 17:10:10 -0400

    fin

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
+from json import JSONDecodeError
 from string import ascii_uppercase, digits
 from os import system, name
 from random import choice
 from requests import get
 from sys import maxsize
 from time import sleep
+import bcolors
 import config
 
 used_vanity = []
 
 def clearConsole():
-    command = 'clear'
-    if name in ('nt', 'dos'):
-        command = 'cls'
-    system(command)
+	command = 'clear'
+	if name in ('nt', 'dos'):
+		command = 'cls'
+	system(command)
 
-def randomString(size=8, chars=ascii_uppercase + digits + "_" + "-"):
+def startupCheck():
+	check = get(f'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key={config.steamAPIKey}&vanityurl=')
+	if check.status_code == 403:
+		print(f'{bcolors.WAITMSG}Steam API Key is invalid. Please check config.py{bcolors.ENDC}')
+		exit()
+
+def randomString(size=8, chars=ascii_uppercase + digits + '_' + '-'):
 	return ''.join(choice(chars) for _ in range(size))
 
-def main():
-	clearConsole()
-	print('How many characters would you like to search for?')
-	amount = int(input())
+def start(amount):
+	search(amount)
+
+def search(amount):
 	for _ in range(maxsize**10):
 		vanityURL = randomString(amount)
 		if vanityURL in used_vanity:
 			vanityURL = randomString(amount)
-		resp = get(f"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={config.steamAPIKey}&vanityurl={vanityURL}").json()
+		try:
+			resp = get(f'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key={config.steamAPIKey}&vanityurl={vanityURL}').json()
+		except JSONDecodeError:
+			start(amount)
 		if resp['response']['success'] == 1:
-			print(f'\033[91mVanity URL Taken: {vanityURL}\033[0m')
+			print(f'{bcolors.ERRMSG}Vanity URL Taken: {vanityURL}{bcolors.ENDC}')
 			used_vanity.append(vanityURL) 
 		else:
-			print(f'\033[92mAvalible Vanity URL found: {vanityURL}\033[0m')
+			print(f'{bcolors.OKMSG}Avalible Vanity URL found: {vanityURL}{bcolors.ENDC}')
 			break
 		sleep(config.delay)
-		
+
+def main():
+	clearConsole()
+	startupCheck()
+	print('How many characters would you like to search for?')
+	amount = int(input())
+	start(amount)
+
 main()
